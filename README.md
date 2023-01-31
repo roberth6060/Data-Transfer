09440f704fe82b659f5eda2c41fa9dc042afefbe
Robert C. Haye <roberthaye1994@gmail.com>
2023-01-31 14:49:40 +0100

    completed script

---
+++ README.md
@@ -0,0 +1,28 @@
+## Summary
+
+Python 3 script that extracts data from local excel file and insert the data in a postgres database table.
+
+## Requirements
+
+Write a Python 3 function which will extract data from the first sheet, transform it and load it to a
+Postgres database table. You need to create the table first according to the excel report fields.
+Do not load (sub)totals. The function should be able to load data from excel reports with the
+same structure, but different number of rows.
+
+### *Installation process* :
+
+- clone repo
+- navigate to project root folder
+- install all dependencies with yarn or npm i
+- run with yarn start or npm
+
+```bash
+
+$ git clone https://github.com/roberth6060/opulent-cars.git
+$ cd project folder
+```
+
+
+## Author(s)
+
+* Robert C. Haye
---
+++ src/data_transfer.py
@@ -0,0 +1,39 @@
+import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy import URL
+import os
+
+# Environment variables
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_host = os.environ.get('DB_HOST')
+
+url_object = URL.create(
+    "postgresql+psycopg2",
+    username=db_username,
+    password=db_password,
+    host=db_host,
+    database='SpaceNK',
+)
+
+# Method - for extracting data from first sheet in excel file and load it to postgres database table
+
+
+def extract_transform_load(file_path, table_name):
+
+    engine = create_engine(url_object)
+
+    # Load data from the first sheet of the Excel report
+    with pd.ExcelFile(file_path) as xls:
+        df = pd.read_excel(xls, sheet_name=0, header=5,
+                           usecols="C,D,F,G,G,I,J,K,L,M,N,O")
+    # Remove (sub)totals
+    df = df[df['Store No'] != 'Total']
+    # Create the table in the database
+    df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+
+
+# Run function with params
+extract_transform_load("assets/docs/SpaceNK_2.0.xlsx",
+                       "Last Week Report by Store Ran")
